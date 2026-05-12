40a460f39310d67560fe4304aacea4968642c777
Roo <roo@antigraviti.com>
2026-05-12 22:27:42 +0700

    fix(db): correct table references in migration

--- database/migrations/20260512_expand_auth_identifiers.sql
+++ database/migrations/20260512_expand_auth_identifiers.sql
@@ -1,13 +1,9 @@
 -- LANCAR EXPAND AUTH IDENTIFIER LIMITS
 -- Date: 2026-05-12
 -- +goose Up
-ALTER TABLE public.customers ALTER COLUMN phone_number TYPE VARCHAR(255);
-ALTER TABLE public.staff ALTER COLUMN phone_number TYPE VARCHAR(255);
-ALTER TABLE public.couriers ALTER COLUMN phone_number TYPE VARCHAR(255);
+ALTER TABLE public.users ALTER COLUMN phone_number TYPE VARCHAR(255);
 ALTER TABLE public.otp_logs ALTER COLUMN phone_number TYPE VARCHAR(255);
 
 -- +goose Down
-ALTER TABLE public.customers ALTER COLUMN phone_number TYPE VARCHAR(20);
-ALTER TABLE public.staff ALTER COLUMN phone_number TYPE VARCHAR(20);
-ALTER TABLE public.couriers ALTER COLUMN phone_number TYPE VARCHAR(20);
+ALTER TABLE public.users ALTER COLUMN phone_number TYPE VARCHAR(20);
 ALTER TABLE public.otp_logs ALTER COLUMN phone_number TYPE VARCHAR(20);
