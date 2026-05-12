ccb1bfa5832a0877cb14fa145b4bd397304db01b
Roo <roo@antigraviti.com>
2026-05-12 22:24:11 +0700

    fix(db): add missing goose migration annotations

--- database/migrations/20260512_expand_auth_identifiers.sql
+++ database/migrations/20260512_expand_auth_identifiers.sql
@@ -1,10 +1,13 @@
 -- LANCAR EXPAND AUTH IDENTIFIER LIMITS
 -- Date: 2026-05-12
-BEGIN;
-
+-- +goose Up
 ALTER TABLE public.customers ALTER COLUMN phone_number TYPE VARCHAR(255);
 ALTER TABLE public.staff ALTER COLUMN phone_number TYPE VARCHAR(255);
 ALTER TABLE public.couriers ALTER COLUMN phone_number TYPE VARCHAR(255);
 ALTER TABLE public.otp_logs ALTER COLUMN phone_number TYPE VARCHAR(255);
 
-COMMIT;
+-- +goose Down
+ALTER TABLE public.customers ALTER COLUMN phone_number TYPE VARCHAR(20);
+ALTER TABLE public.staff ALTER COLUMN phone_number TYPE VARCHAR(20);
+ALTER TABLE public.couriers ALTER COLUMN phone_number TYPE VARCHAR(20);
+ALTER TABLE public.otp_logs ALTER COLUMN phone_number TYPE VARCHAR(20);
